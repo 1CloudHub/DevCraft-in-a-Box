3f1153e0bee48007ffef66c178312ea6f12f991b
Hakash1CH <hakash.mp@1cloudhub.com>
2026-03-26 22:48:15 +0530

    Updated the Bucket Naming

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -263,7 +263,8 @@ class CdkStack(Stack):
         
         
         # === Create S3 ===
-        bucket_name = "cexp-ocr-bucket"
+        cexp_bucket_suffix = ''.join(random.choices(string.ascii_lowercase + string.digits, k=45))
+        bucket_name = "cexp-ocr-bucket-" + cexp_bucket_suffix
         final_bucket_name = get_available_bucket_name(bucket_name)
 
         # Create the bucket with your original CORS config
@@ -674,7 +675,8 @@ class CdkStack(Stack):
         
 
         # === Create S3 ===
-        site_bucket = "cexp-ocr-site"
+        site_bucket_suffix = ''.join(random.choices(string.ascii_lowercase + string.digits, k=45))
+        site_bucket = "cexp-ocr-site-" + site_bucket_suffix
         site_bucket_name = get_available_bucket_name(site_bucket)
 
         # Create the bucket with your original CORS config
